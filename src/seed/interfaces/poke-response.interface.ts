acba33ee65eeb653a72d771f4f19f646837f207c
VazquezAriel <ariel.renato.av@gmail.com>
2022-12-08 15:15:22 -0500

    Creacion de modulo seed para agregar datos por lote

---
+++ src/seed/interfaces/poke-response.interface.ts
@@ -0,0 +1,12 @@
+
+export interface PokeResponce {
+    count:    number;
+    next:     string;
+    previous: null;
+    results:  Result[];
+}
+
+export interface Result {
+    name: string;
+    url:  string;
+}
