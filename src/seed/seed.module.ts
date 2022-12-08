a79dae938ad9ab866069de843b12cd076d644b1a
VazquezAriel <ariel.renato.av@gmail.com>
2022-12-08 15:58:08 -0500

    Finalizacion del endpoint seed

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
+import { PokemonModule } from '../pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   controllers: [SeedController],
-  providers: [SeedService]
+  providers: [SeedService],
+  imports: [PokemonModule]
+
 })
 export class SeedModule {}
